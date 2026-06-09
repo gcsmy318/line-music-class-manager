5e34736f7e40b0c4e76270a2b4f2b2f329576a2d
Chatchapon Silarak <chatchaponsil@gosoft.co.th>
2026-06-09 22:25:00 +0700

    first commit

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -4,11 +4,10 @@ admin.initializeApp({
   credential: admin.credential.cert({
     projectId: process.env.FIREBASE_PROJECT_ID,
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(
-      /\\n/g,
-      "\n"
-    ),
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
   }),
 });
 
-module.exports = admin.firestore();
+const db = admin.firestore();
+
+module.exports = { admin, db };
