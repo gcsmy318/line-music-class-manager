d62185046e2777bed379fa25b5e8b84adf05fa87
Chatchapon Silarak <chatchaponsil@gosoft.co.th>
2026-06-10 00:56:30 +0700

    temporarily disable line signature check

--- src/server.js
+++ src/server.js
@@ -20,7 +20,7 @@ app.use(cors());
 app.use('/public', express.static(path.join(__dirname,'public')));
 app.use(cookieParser());
 app.use(express.urlencoded({extended:true, limit:'10mb'}));
-app.use(express.json({ verify: verifySignature, limit:'10mb' }));
+app.use(express.json({ limit:'10mb' }));
 app.use(methodOverride('_method'));
 
 app.use(session({
